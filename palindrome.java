d0b8091aff73dd6ace1e2c77c95311c0f5aecf29
Student <Student@nwu.ac.za>
2022-08-10 16:39:07 +0200

    Change me to README file

---
+++ palindrome.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of class palindrome here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+import java.util.Scanner;
+public class palindrome
+{
+    public static void main(String args[])
+    {
+        String original, reverse = ""; // Objects of String class.
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter a string/number to check if it is a palindrome");
+    }
+}
